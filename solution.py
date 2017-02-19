3561281ae68909ebae287d83039f203ad1f06799
Arkadiusz Komarzewski <ak.komar@gmail.com>
2017-02-19 10:35:38 +0100

    Extract utils to separate file, use logging for debugging

--- solution.py
+++ solution.py
@@ -1,27 +1,8 @@
 from collections import defaultdict
 
+from utils import *
+
 assignments = []
-
-rows = 'ABCDEFGHI'
-cols = '123456789'
-
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    return [s + t for s in A for t in B]
-
-
-boxes = cross(rows, cols)
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-diagonal_units = [[r + c for (r, c) in zip(rows, cols)], [r + c for (r, c) in zip(reversed(rows), cols)]]
-unitlist = row_units + column_units + square_units + diagonal_units
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-
-peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
-
-print(diagonal_units)
 
 
 def assign_value(values, box, value):
@@ -171,6 +152,7 @@ if __name__ == '__main__':
 
     try:
         from visualize import visualize_assignments
+
         visualize_assignments(assignments)
 
     except SystemExit:
